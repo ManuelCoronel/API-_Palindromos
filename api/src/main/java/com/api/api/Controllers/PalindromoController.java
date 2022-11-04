723dbb12107f434ddaf57683ea885b44144b3c5e
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-11-04 13:15:01 -0500

    Metodo de validar palindromos creado

--- api/src/main/java/com/api/api/Controllers/PalindromoController.java
+++ api/src/main/java/com/api/api/Controllers/PalindromoController.java
@@ -1,8 +1,15 @@
 package com.api.api.Controllers;
+import com.api.api.Services.PalindromoCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class PalindromoController {
 
+
+    @Autowired
+    private PalindromoCheckService service;
+
+    
 }
--- api/src/main/java/com/api/api/Services/PalindromoCheckService.java
+++ api/src/main/java/com/api/api/Services/PalindromoCheckService.java
@@ -8,11 +8,28 @@ public class PalindromoCheckService implements IValidatePalindrome {
 
     @Override
     public String validatePlaindrome(String word) {
-    return null;
+    String reverseWord = getReverseWord(word);
+    String palindrome = "";
+        for(int i=0;i<reverseWord.length();i++){
+            for(int j=i+1;j<reverseWord.length();j++){
+                if(word.substring(i,j).equals(reverseWord.substring(reverseWord.length()-j,reverseWord.length()-i))){
+                    if (word.substring(i,j).length()>palindrome.length()){
+                        palindrome=word.substring(i,j);
+                    }
+                }
+            }
+        }
+        return palindrome;
     }
 
     @Override
     public String getReverseWord(String word) {
-        return null;
+        String newWord="";
+
+        for (int i= newWord.length()-1;i>=0;i--){
+            newWord += word.charAt(i);
+
+        }
+        return newWord;
     }
 }
