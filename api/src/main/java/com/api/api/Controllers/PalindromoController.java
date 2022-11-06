1625ebcc494003c84d96d95fb882e51589d49977
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-11-05 19:14:02 -0500

    Response con response entity

--- api/src/main/java/com/api/api/Controllers/PalindromoController.java
+++ api/src/main/java/com/api/api/Controllers/PalindromoController.java
@@ -2,6 +2,9 @@ package com.api.api.Controllers;
 import com.api.api.Contracts.Palindrome;
 import com.api.api.Services.PalindromoCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +15,15 @@ public class PalindromoController {
     private PalindromoCheckService service;
 
     @PostMapping("/palindrome")
-        public Palindrome validatePalindrome (@RequestBody Palindrome palindrome){
-            return new Palindrome(service.validatePlaindrome(palindrome.getPalindrome()));
+        public ResponseEntity<Palindrome> validatePalindrome (@RequestBody Palindrome palindrome){
+        try {
+
+            return  ResponseEntity.accepted().body(new Palindrome(service.validatePlaindrome(palindrome.getPalindrome())));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(new Palindrome("No se ha encontrado palindromo"));
+
+        }
+
         }
 }
 
